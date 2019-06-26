b5494c6829943c7e8582690b254370dc3083f792
Rozeena Maharjan <rozeena.maharjan2000@gmail.com>
2019-06-26 22:04:38 +0545

    farmers details

--- lib/farmers/farmerdetails.dart
+++ lib/farmers/farmerdetails.dart
@@ -1,30 +1,93 @@
 import 'package:flutter/material.dart';
 
-class FarmersDetails extends StatefulWidget {
-  FarmersDetails({Key key}) : super(key: key);
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FarmersDetails(),
+    );
+  }
+}
+
+class FarmersDetails extends StatefulWidget {
+  @override
   _FarmersDetailsState createState() => _FarmersDetailsState();
 }
 
 class _FarmersDetailsState extends State<FarmersDetails> {
-   Widget _buildBackground() {
-    return new Image.asset(
-      'asset/brad1.jpg',
-      fit: BoxFit.cover,
-    );
-  }
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(
-        title: new Text("Kisan ko jankari"),
-      ),
       body: ListView(
+        shrinkWrap: true,
         children: <Widget>[
-          _buildBackground(),
-       
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Container(
+                    height: 275.0,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("images/farmer.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              "Name:",
+              style: TextStyle(color: Colors.blue, fontSize: 20.0),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              "Location:",
+              style: TextStyle(color: Colors.blue, fontSize: 20.0),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all( 10.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Text(
+                "Phone number: ",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          ),
+           Padding(
+               padding: EdgeInsets.all(10),
+             child: new RaisedButton(
+               textColor: Colors.blue,
+               elevation: 1,
+               onPressed:
+             (){},
+                 child: new Text("Contacts"),
+
+
+
+
+
+
+
+
+
+             ),
+           )
+
         ],
-        
       ),
     );
   }
